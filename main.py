124e02fbc9a50ec0c575d94d1cbd10e1d5976110
leeyunhobbang <129865380+leeyunhobbang@users.noreply.github.com>
2023-04-20 00:17:13 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome() -> dict:
+  return {
+      "msg" : "Hello world!"
+      }
+     
+if __name__ == '__main__':
+  uvicorn.run("main:app", host= "0.0.0.0", port = 8000, reload = Ture)
